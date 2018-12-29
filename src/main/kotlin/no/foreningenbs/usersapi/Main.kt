abf7cc61e0f4e5d83935b3a8e829a1affd985619
Henrik Steen <henrist@henrist.net>
2018-12-29 17:06:43 +0100

    Log details for lens failures

--- src/main/kotlin/no/foreningenbs/usersapi/Main.kt
+++ src/main/kotlin/no/foreningenbs/usersapi/Main.kt
@@ -16,6 +16,7 @@ import org.http4k.core.HttpHandler
 import org.http4k.core.Method.GET
 import org.http4k.core.Method.POST
 import org.http4k.core.Response
+import org.http4k.core.Status.Companion.BAD_REQUEST
 import org.http4k.core.Status.Companion.OK
 import org.http4k.core.then
 import org.http4k.filter.ServerFilters
@@ -79,6 +80,9 @@ fun app(ldap: Ldap, dataProvider: DataProvider): HttpHandler {
       ))
   )
 
-  return ServerFilters.CatchLensFailure()
+  return ServerFilters.CatchLensFailure {
+    logger.debug("Lens failure: ${it.message}", it)
+    Response(BAD_REQUEST.description(it.failures.joinToString("; ")))
+  }
     .then(routes)
 }
