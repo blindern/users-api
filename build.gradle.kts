0f3c36fbe4934e2bc9ddff7085aa2125fa84ac0a
Henrik Steen <henrist@henrist.net>
2019-05-10 19:13:02 +0200

    Update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,17 +1,16 @@
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 import de.fuerstenau.gradle.buildconfig.BuildConfigSourceSet
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-import org.gradle.jvm.tasks.Jar
 import java.io.ByteArrayOutputStream
 import java.time.Instant
 import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
 
 plugins {
   application
-  kotlin("jvm") version "1.3.11"
-  id("com.github.johnrengelman.shadow") version "4.0.3"
-  id("org.jlleitschuh.gradle.ktlint") version "6.3.1"
-  id("com.github.ben-manes.versions") version "0.20.0"
+  kotlin("jvm") version "1.3.31"
+  id("com.github.johnrengelman.shadow") version "5.0.0"
+  id("org.jlleitschuh.gradle.ktlint") version "8.0.0"
+  id("com.github.ben-manes.versions") version "0.21.0"
   id("de.fuerstenau.buildconfig") version "1.1.8"
 }
 
@@ -20,7 +19,7 @@ version = "1.0-SNAPSHOT"
 
 buildscript {
   dependencies {
-    classpath("com.karumi.kotlinsnapshot:plugin:2.0.0")
+    classpath("com.karumi.kotlinsnapshot:plugin:2.1.0")
   }
 }
 
@@ -47,23 +46,23 @@ repositories {
 
 dependencies {
   implementation(kotlin("stdlib-jdk8"))
-  implementation("org.http4k", "http4k-core", "3.103.2")
-  implementation("org.http4k", "http4k-server-jetty", "3.103.2")
-  implementation("org.http4k", "http4k-format-moshi", "3.103.2")
+  implementation("org.http4k", "http4k-core", "3.141.0")
+  implementation("org.http4k", "http4k-server-jetty", "3.141.0")
+  implementation("org.http4k", "http4k-format-moshi", "3.141.0")
   implementation("com.natpryce", "konfig", "1.6.10.0")
   implementation("com.squareup.moshi", "moshi", "1.8.0")
   implementation("com.squareup.moshi", "moshi-kotlin", "1.8.0")
-  implementation("org.jetbrains.kotlin", "kotlin-stdlib-jdk8", "1.3.11")
-  implementation("org.jetbrains.kotlin", "kotlin-reflect", "1.3.11")
-  implementation("com.github.ben-manes.caffeine", "caffeine", "2.6.2")
+  implementation("org.jetbrains.kotlin", "kotlin-stdlib-jdk8", "1.3.31")
+  implementation("org.jetbrains.kotlin", "kotlin-reflect", "1.3.31")
+  implementation("com.github.ben-manes.caffeine", "caffeine", "2.7.0")
   implementation("ch.qos.logback", "logback-classic", "1.2.3")
   implementation("ch.qos.logback.contrib", "logback-json-classic", "0.1.5")
   implementation("de.gessnerfl.logback", "logback-gson-formatter", "0.1.0")
-  implementation("io.github.microutils", "kotlin-logging", "1.6.22")
-  testImplementation("org.amshove.kluent", "kluent", "1.45")
-  testImplementation("org.spekframework.spek2", "spek-dsl-jvm", "2.0.0-rc.1")
-  testImplementation("io.mockk", "mockk", "1.8.13.kotlin13")
-  testRuntimeOnly("org.spekframework.spek2", "spek-runner-junit5", "2.0.0-rc.1")
+  implementation("io.github.microutils", "kotlin-logging", "1.6.26")
+  testImplementation("org.amshove.kluent", "kluent", "1.49")
+  testImplementation("org.spekframework.spek2", "spek-dsl-jvm", "2.0.4")
+  testImplementation("io.mockk", "mockk", "1.9.3")
+  testRuntimeOnly("org.spekframework.spek2", "spek-runner-junit5", "2.0.4")
 }
 
 // The idea plugin makes generated BuildConfig resolved
